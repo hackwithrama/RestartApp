f02e198a53fc47080a00c52db89cfad63a1c3444
hackwithrama <rajram92@hotmail.com>
2024-06-28 14:12:40 +0530

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Ramachandran Varadaraju on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
